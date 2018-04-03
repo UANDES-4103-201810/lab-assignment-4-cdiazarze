ed2b95ec170e5561ebebdf6605c2231d6883bf49
cdiazarze <cdiaz1@miuandes.cl>
2018-04-03 14:58:09 -0300

    Avance 1

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :place
   has_many :tickets
+  validates :start_date, if: :validate_date?
+
+  def validate_date?
+    :start_date>:created_at
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   	has_many :user_tickets	
-	has_many :tickets, :through => :user_tickets
+		has_many :tickets, :through => :user_tickets
+		validates_format_of :email,:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})+\z/
+		validates_format_of :phone,:with => /[0-9]{9,12}/
 end
